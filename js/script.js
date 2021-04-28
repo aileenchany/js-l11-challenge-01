471f3d9bc7be206e2426789cfdb8d3c5c3e7ddd4
Aileen CM <aileenchany@gmail.com>
2021-04-28 17:37:15 -0400

    added js functions to select ranmdom image, display the image,and click event

--- js/script.js
+++ js/script.js
@@ -4,8 +4,29 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function() {
-    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const res = await fetch("https://picsum.photos/v2/list?limit=100"); //the api url gives a limit of 100 images
     const images = await res.json();
-    console.log(images);
-}
-getImage();
+    //console.log(images);
+    selectRandomImage(images);
+};
+
+
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length); //gets a random image number between 0 and 99
+    const randomImage = images[randomIndex]; //gets the random image data formatted as an object
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function(randomImage) {
+    const author = randomImage["author"];
+    const imageUrl = randomImage["download_url"];
+    authorSpan.innerText = `${author}`;
+    img.src = imageUrl;
+    imgDiv.classList.remove("hide");
+};
+
+
+button.addEventListener("click", function() {
+    getImage();
+});
